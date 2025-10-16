0d240d58785408c0413bfa949da724b50aff45d1
shanib-techie <shanibkhan622@gmail.com>
2025-10-16 15:51:07 +0530

    add link basic

--- link-list-basic.cpp
+++ link-list-basic.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 using namespace std;
-int main(){
 
-    cout<<"ELLO WORD";
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = NULL;
+    }
+
+    // Function to insert node at end
+    void insertAtEnd(int value) {
+        Node* newNode = new Node(value);
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    // Function to display linked list
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+
+    cout << "Linked List: ";
+    list.display();
+
+    return 0;
 }
