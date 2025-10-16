2868d5a7fd99648fbaf2679c8f2ad37400445dbd
shanib-techie <shanibkhan622@gmail.com>
2025-10-16 16:38:32 +0530

    change

--- insertion_in_link_listed.cpp
+++ insertion_in_link_listed.cpp
@@ -2,34 +2,39 @@
 using namespace std;
 
 
-class Node{
-    public:
-    int val;
-    Node* next;
+// INSERTED AT HEAD 
 
-    Node(int data){
-        val = data;
-        next = NULL;
+
+class Node{  //ek node me do part hote h ek val and second in next addres .address pointer me hota h sp humne node* use kra h
+    public:
+    int val; 
+    Node* next;
+    
+
+
+    Node(int data){    //cpnstructor for node
+        this->val = data;
+        this->next = NULL;
     }
 };
 
-void insertathead(Node* &head, int val){
+void insertathead(Node* &head, int val){  //node* &head matlb hamari link listed, int val matlb uski value
     Node* new_node = new Node(val);
     new_node->next = head;
     head = new_node;
 }
 void display(Node* head){
-    Node*temp = head;
-    while(temp!=NULL){
-        cout<<temp->val<<"->";
-        temp = temp->next;
+    Node*temp = head; // ek temp ka variable me head daalo matlb address vo second part hota h
+    while(temp!=NULL){  //jab tak node me jo second part wani address null naa ho jab tak yeh loop cahalao
+        cout<<temp->val<<"->";//jab tak loop chl rha h temp ki value {data print karo jo node me h }
+        temp = temp->next;  
     }
     cout<<"NULL"<<endl;
 }
 int main(){
-    Node* head = NULL;
-    insertathead(head,2);
-    display(head);
-    insertathead(head,1);
+    Node* head = NULL;  //head matlb hamari node h jo ki abhi null ha
+    insertathead(head,2);//head matlb node me hmne 2 insert krdiya
+    display(head);//abb print kari so 2 mil gya
+    insertathead(head,1);//abb dubara isrert kra
     display(head);
 }
--- link-list-basic.cpp
+++ link-list-basic.cpp
@@ -1,80 +1,80 @@
 #include <iostream>
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node* next;
-
-    Node(int value) {
-        data = value;
-        next = NULL;
-    }
-};
-
-class LinkedList {
-public:
-    Node* head;
-
-    LinkedList() {
-        head = NULL;
-    }
-
-    // Function to insert node at end
-    void insertAtEnd(int value) {
-        Node* newNode = new Node(value);
-        if (head == NULL) {
-            head = newNode;
-            return;
-        }
-
-        Node* temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-    }
-
-    // Function to display linked list
-    void display() {
-        Node* temp = head;
-        while (temp != NULL) {
-            cout << temp->data << " -> ";
-            temp = temp->next;
-        }
-        cout << "NULL" << endl;
-    }
-};
-
-int main() {
-    LinkedList list;
-
-    list.insertAtEnd(10);
-    list.insertAtEnd(20);
-    list.insertAtEnd(30);
-
-    cout << "Linked List: ";
-    list.display();
-
-    return 0;
-}
-
 
 
 
 
 
 // class Node{
-//     public:
-//     int val;
-//     Node* next;
-
-//     Node(int data){
-//         val = data;
-//         next = NULL;
-//     }
-// };
-// int main(){
-//     Node* n = new Node(1);
-//     cout<<n->val<<" "<<n->next<<endl;
-// }
+    //     public:
+    //     int val;
+    //     Node* next;
+    
+    //     Node(int data){
+        //         val = data;
+        //         next = NULL;
+        //     }
+        // };
+        // int main(){
+            //     Node* n = new Node(1);
+            //     cout<<n->val<<" "<<n->next<<endl;
+            // }
+            
+            // class Node {
+            // public:
+            //     int data;
+            //     Node* next;
+            
+            //     Node(int value) {
+            //         data = value;
+            //         next = NULL;
+            //     }
+            // };
+            
+            // class LinkedList {
+            // public:
+            //     Node* head;
+            
+            //     LinkedList() {
+            //         head = NULL;
+            //     }
+            
+            //     // Function to insert node at end
+            //     void insertAtEnd(int value) {
+            //         Node* newNode = new Node(value);
+            //         if (head == NULL) {
+            //             head = newNode;
+            //             return;
+            //         }
+            
+            //         Node* temp = head;
+            //         while (temp->next != NULL) {
+            //             temp = temp->next;
+            //         }
+            //         temp->next = newNode;
+            //     }
+            
+            //     // Function to display linked list
+            //     void display() {
+            //         Node* temp = head;
+            //         while (temp != NULL) {
+            //             cout << temp->data << " -> ";
+            //             temp = temp->next;
+            //         }
+            //         cout << "NULL" << endl;
+            //     }
+            // };
+            
+            // int main() {
+            //     LinkedList list;
+            
+            //     list.insertAtEnd(10);
+            //     list.insertAtEnd(20);
+            //     list.insertAtEnd(30);
+            
+            //     cout << "Linked List: ";
+            //     list.display();
+            
+            //     return 0;
+            // }
